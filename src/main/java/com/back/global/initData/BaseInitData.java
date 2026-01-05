532e9d93e3bbd86d2e428557fe4e24a0c4deaf73
juyeongkyeong <juyk.oung@gmail.com>
2026-01-05 15:50:25 +0900

    009

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +10,19 @@ import org.springframework.context.annotation.Configuration;
 // @Configuration 어노테이션으로 설정 클래스 지정
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class BaseInitData {
+    private final PostService postService;
 
     @Bean
     public ApplicationRunner baseInitDataRunner (){
         // ApplicationRunner : Bean을 통해 애플리케이션 시작 시 초기화 로직 실행
         return args->{
-            log.debug("ApplicationRunner 빈은 스프링에 등록되면 자동으로 실행됩니다");
+            work1();
         };
+    }
+
+    private void work1() {
+        log.debug("Post entity 개수: {}", postService.count());
     }
 }
